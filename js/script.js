865e43f81acf40ac2eb7a5cbfb1df4507373adc9
Alex <alexhagge128@gmail.com>
2016-03-23 20:23:49 -0700

    started ux logic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-//source code
-
+//Business Logic
 function bankAccount(name, amount) {
   this.name = name;
   this.amount = amount;
+}
+
+bankAccount.prototype.withdrawl = function(withdrawl) {
+  this.amount = this.amount - parseInt(withdrawl);
+  return this.amount;
 }
 
 bankAccount.prototype.deposit = function(deposit) {
@@ -10,7 +14,12 @@ bankAccount.prototype.deposit = function(deposit) {
   return this.amount;
 }
 
-bankAccount.prototype.withdrawl = function(withdrawl) {
-  this.amount = this.amount - parseInt(withdrawl);
-  return this.amount;
-}
+//User Logic
+  $(document).ready(function() {
+    $("form#accountInfo").submit(function(event) {
+      var newName = $("input#name").val()
+      var newDeposit = $("input#initialDeposit").val()
+      var result = new bankAccount(newName, newDeposit);
+      
+    })
+  })
